95126f1eab15ae8a414e74b6213c2a5e42e6568b
Hamik Hambardzumyan <hamik.hambardzumyan@softconstruct.com>
2024-10-24 21:07:08 +0400

    Remove index.js and create it in preBuild script, add declaration support for bootstrap function

--- scripts/preBuild.js
+++ scripts/preBuild.js
@@ -1,6 +1,29 @@
-import { rm } from 'fs/promises';
+import { rm, mkdir, writeFile } from 'fs/promises';
 import { resolve } from 'path';
 
 (async () => {
-    await rm(resolve(__dirname, '../dist'), { recursive: true, force: true });
+    const distPath = resolve(__dirname, '../dist');
+    const indexTsContent = `
+import * as jsTokens from './js/tokens';
+
+export const bootstrap = () => {
+    import('./css/tokens.css');
+    return jsTokens;
+};
+    `;
+
+    try {
+        // Remove the dist directory if it exists
+        await rm(distPath, { recursive: true, force: true });
+
+        // Create the dist directory
+        await mkdir(distPath, { recursive: true });
+
+        // Create the index.ts file with the specified content
+        await writeFile(resolve(distPath, 'index.js'), indexTsContent.trim());
+
+        console.log('Dist directory recreated and index.js file created.');
+    } catch (error) {
+        console.error('Error during prebuild process:', error);
+    }
 })();
